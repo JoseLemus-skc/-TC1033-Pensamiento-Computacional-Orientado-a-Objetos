9db8af14afd931e7ad36b1214c804bdbf2251081
Jose Lemus <A01707194@tec.mx>
2025-11-14 10:44:53 -0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <string>
-// #include <inst>
-// #include <btrack>
-// #include <micr>
+#include "inst"
+#include "btrack"
+#include "micr"
 
 int main()
 {
-    std::string lineNumber("");
-    std::cout << "Introduce el numero de linea" << std::endl;
-    std::cin >> lineNumber;
-    std::cout << lineNumber << std::endl;
+
 }
