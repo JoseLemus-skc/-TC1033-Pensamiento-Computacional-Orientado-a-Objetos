5816d02bf1b870b31493fd468b569c0efa4a2204
Jose Lemus <A01707194@tec.mx>
2025-11-28 08:35:42 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,10 +1,79 @@
 #include <iostream>
 #include <string>
-#include "inst"
-#include "btrack"
-#include "micr"
+#include "email.h"
+#include "promospam.h"
+#include "purchaseconfirmation.h"
+#include "tickets.h"
 
-int main()
-{
+int main() {
+    int opcion;
 
+    std::cout << "1) Crear PromoSpam\n";
+    std::cout << "2) Crear Confirmacion Compra\n";
+    std::cout << "3) Crear Tickets\n";
+    std::cout << "Opción: ";
+    std::cin >> opcion;
+
+    if (opcion == 1) {
+        std::string code, receiver, time;
+        std::cout << "Codigo promocional: ";
+        std::cin >> code;
+
+        std::cout << "Direccion de correo a enviar: ";
+        std::cin >> receiver;
+
+        std::cout << "Fecha: ";
+        std::cin >> time;
+
+        PromoSpam spam(code, receiver, time);
+        std::cout << spam.emailDraft() << "\n";
+    }
+
+    else if (opcion == 2) {
+        float money;
+        std::string orderID, receiver, time;
+
+        std::cout << "Dinero gastado: ";
+        std::cin >> money;
+
+        std::cout << "Numero de la orden: ";
+        std::cin >> orderID;
+
+        std::cout << "Direccion de correo a enviar: ";
+        std::cin >> receiver;
+
+        std::cout << "Fecha: ";
+        std::cin >> time;
+
+        PurchaseConfirmation confirmacion(money, orderID, receiver, time);
+        std::cout << confirmacion.emailDraft() << "\n";
+    }
+
+    else if (opcion == 3) {
+        std::string concert, artist, code, receiver, time;
+
+        std::cout << "Concierto: ";
+        std::cin >> concert;
+
+        std::cout << "Artista: ";
+        std::cin >> artist;
+
+        std::cout << "Codigo del ticket: ";
+        std::cin >> code;
+
+        std::cout << "Direccion de correo a enviar: ";
+        std::cin >> receiver;
+
+        std::cout << "Fecha: ";
+        std::cin >> time;
+
+        Tickets boletos(concert, artist, code, receiver, time);
+        std::cout << boletos.emailDraft() << "\n";
+    }
+
+    else {
+        std::cout << "Opcion invalida.\n";
+    }
+
+    return 0;
 }
