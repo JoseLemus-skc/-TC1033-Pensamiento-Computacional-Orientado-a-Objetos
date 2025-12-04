b37457a81fea36227857dd9b0c0038986ba45068
Jose Lemus <A01707194@tec.mx>
2025-12-04 02:21:53 -0600

    Tickets class

--- tickets.h
+++ tickets.h
@@ -7,12 +7,15 @@
 
 class Tickets: public Email{
     private:
+    // Atributos
     std::string concertName;
     std::string artistName;
     std::string ticketCode;
     bool isValid;
 
     // Procesamiento del Subject y Body
+    std::string bodyDateFormat();
+
     std::string updateSubject();
     std::string updateBody(); 
 
@@ -20,12 +23,12 @@ class Tickets: public Email{
     void setBody();
 
     public:
-    // Constructors
-    Tickets(): Email("", "", ""), concertName(""), artistName(""), ticketCode(""), isValid(false){
+    // Constructores
+    Tickets(): Email("", "", 23, 31, 12, 1999), concertName(""), artistName(""), ticketCode(""), isValid(false){
         setSubject();
         setBody();
     }
-    Tickets(std::string concert, std::string artist, std::string code, std::string receiver, std::string time): Email(receiver, time, "Spam Inbox"){
+    Tickets(std::string concert, std::string artist, std::string code, std::string receiver,  short h, short d, short m, short y): Email(receiver, "Spam", h, d, m, y){
         concertName = concert;
         artistName = artist;
         ticketCode = code;
@@ -40,29 +43,38 @@ class Tickets: public Email{
     void setConcert(std::string concert);
     void setArtist(std::string artist);
     void setCode(std::string code);
-    void setValid(bool state); // Corregir a Random
+    void setValid(bool state);
 
     // Getters
     std::string getConcertName();
     std::string getArtistName();
     std::string getCode();
     bool getValidStatus();
+
+    void punishment();
 };
 
 // Definiciones (updaters)
-std::string Tickets::updateSubject(){
+std::string Tickets::bodyDateFormat(){
     std::stringstream aux;
-    aux << artistName << " en " << concertName;
+    aux << sendTime.getDay() << " de " << sendTime.getMonthName();
     return aux.str();
 }
+
+std::string Tickets::updateSubject(){
+    std::stringstream aux;
+    aux << "Tus Tickets para " << artistName << " en " << concertName;
+    return aux.str();
+}
+
 std::string Tickets::updateBody(){
     std::stringstream aux;
     if (isValid == false){
-        aux << "Fecha del concierto: " << sendTime << "\n Estimado Fan: \n Recuerda descargar tus boletos [INVALIDOS] SafeTix con codigo "
-        << ticketCode << " agregalos a tu Wallet y de esta forma estaran disponibles aun sin conexion a internet";
+        aux << "\nEstimado Fan: \nRecuerda que no tienes que descargar tus boletos SafeTix con codigo "
+        << ticketCode << " para " << artistName << " este " << bodyDateFormat() << " porque son invalidos\nNo es necesario agregarlos a tu Wallet. Byeeeee";
     } else{
-        aux << "Fecha del concierto: " << sendTime << "\n Estimado Fan: \n Recuerda descargar tus boletos safeTix con codigo "
-        << ticketCode << " agregalos a tu Wallet y de esta forma estaran disponibles aun sin conexion a internet";
+        aux << "\nEstimado Fan: \nRecuerda descargar tus boletos SafeTix con codigo " << ticketCode 
+        << " para " << artistName << " este " << bodyDateFormat() << "\nRecuerda agregarlos a tu Wallet y de esta forma estaran disponibles aun sin conexion a internet";
     }
     return aux.str();
 }
@@ -71,23 +83,29 @@ std::string Tickets::updateBody(){
 void Tickets::setSubject(){
     subject = updateSubject();
 }
+
 void Tickets::setBody(){
     body = updateBody();
 }
+
 void Tickets::setConcert(std::string concert){
     concertName = concert;
     setSubject();
 }
+
 void Tickets::setArtist(std::string artist){
     artistName = artist;
     setSubject();
 }
+
 void Tickets::setCode(std::string code){
     ticketCode = code;
     setBody();
 }
+
 void Tickets::setValid(bool state){
     isValid = state;
+    punishment();
     setBody();
 }
 
@@ -96,14 +114,22 @@ void Tickets::setValid(bool state){
 std::string Tickets::getConcertName(){
     return concertName;
 }
+
 std::string Tickets::getArtistName(){
     return artistName;
 }
+
 std::string Tickets::getCode(){
     return ticketCode;
 }
+
 bool Tickets::getValidStatus(){
     return isValid;
+}
+
+void Tickets::punishment(){
+    emailPriority = "Ninguno. No te toca recibir nada. Byeeeee";
+    deliveryMethod = "Paloma";
 }
 
 #endif
