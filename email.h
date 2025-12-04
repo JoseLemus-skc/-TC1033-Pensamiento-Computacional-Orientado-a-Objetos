f38b4cc65f545e521f762bd5948cb8cea8ccffa3
Jose Lemus <A01707194@tec.mx>
2025-12-04 02:20:18 -0600

    Email class

--- email.h
+++ email.h
@@ -2,44 +2,48 @@
 #define EMAIL_H
 
 #include <string>
+#include <sstream>
+#include "date.h"
 
 class Email{
+private:
+    std::string dateFormat();
+
 protected:
     // Atributos
     std::string subject;
     std::string body;
     std::string senderEmail;
     std::string receiverEmail;
-    std::string sendTime;
+    Date sendTime;
     std::string emailPriority;
     std::string deliveryMethod;
 
 public:
-    // Constructors
+    // Constructores
     Email(){}
-    Email(std::string receiver, std::string date, std::string priority){
+    Email(std::string receiver, std::string priority, short h, short d, short m, short y){
         subject = "";
         body = "";
         senderEmail = "noreply@ticketmaster.com.mx";
         receiverEmail = receiver;
-        sendTime = date;
+        sendTime = Date(h, d, m, y);
         emailPriority = priority;
         deliveryMethod = "Correo electronico";
     }
 
     // Setters
     void setReceiver(std::string address);
-    void setTime(std::string date);
     void setPriority(std::string priority);
 
     // Getters
-    std::string getSubject();
-    std::string getBody();
-    std::string getSender();
-    std::string getReceiver();
-    std::string getTime();
     std::string getPriority();
     std::string getDelivery();
+    std::string getDate();
+    std::string getReceiver();
+
+    std::string getSubject();
+    std::string getBody();
 
     // Boceto de Email
     std::string emailDraft();
@@ -50,40 +54,40 @@ public:
 void Email::setReceiver(std::string address){
     receiverEmail = address;
 }
-void Email::setTime(std::string date){
-    sendTime = date;
-}
 void Email:: setPriority(std::string priority){
     emailPriority = priority;
 }
 
 // Definiciones (Getters)
-std::string Email::getSubject(){
-    return subject;
-}
-std::string Email::getBody(){
-    return body;
-}
-std::string Email::getSender(){
-    return senderEmail;
-}
-std::string Email::getReceiver(){
-    return receiverEmail;
-}
-std::string Email::getTime(){
-    return sendTime;
-}
 std::string Email::getPriority(){
     return emailPriority;
 }
 std::string Email::getDelivery(){
     return deliveryMethod;
 }
+std::string Email::getSubject(){
+    return subject;
+}
+std::string Email::getBody(){
+    return body;
+}
+std::string Email::getReceiver(){
+    return receiverEmail;
+}
+std::string Email::getDate(){
+    return dateFormat();
+}
 
+// Definicion (dateFormat)
+std::string Email::dateFormat(){
+    std::stringstream aux;
+    aux << sendTime.getHour() << " - " << sendTime.getDay() << "/" << sendTime.getMonth() << "/" << sendTime.getYear();  
+    return aux.str();
+}
 
 // Definicion (Boceto de Email)
 std::string Email::emailDraft(){
-    std::string header = "Para: " + receiverEmail + "       Enviado: " + sendTime; 
+    std::string header = "Para: " + receiverEmail + "       Enviado: " + dateFormat(); 
     return header + "\n\n" + subject + "\n" + body  + "\n\n" + senderEmail;
 }
 
